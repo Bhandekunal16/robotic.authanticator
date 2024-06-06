8d9fb65e60bdb397d3f1d332accdbf207b6ec4a7
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-06 16:47:45 +0530

    "Added AboutComponent to app routes and updated favicon link in index.html"

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,49 @@
+import { CommonModule } from '@angular/common';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [HttpClientModule, CommonModule],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss',
+})
+export class AboutComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  public data: any[] = [];
+
+  ngOnInit(): void {
+    this.info().subscribe((ele) => {
+      const obj = ele.data.versions;
+      const arr = [];
+      for (let key in obj) {
+        arr.push({ key: key, value: obj[key] });
+      }
+      this.data = arr;
+    });
+  }
+
+  info(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>(
+        'https://townhall-ten.vercel.app/info',
+        { name: 'robotic-authenticator' },
+        { headers }
+      )
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
+}
